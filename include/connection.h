6cd05beb4cbbe55acc9ee8a91bf7663098f586e7
Relu12345 <aurel.coruian02@e-uvt.ro>
2024-04-20 19:50:07 +0300

    more qol changes

--- include/connection.h
+++ include/connection.h
@@ -2,7 +2,7 @@
 #define CONNECTION_H
 
 #define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket"
-#define BUFFER_SIZE 12
+#define SOCKETS_AVAILABLE 5
 #define MESSAGE_SIZE 50
 
 #endif /* CONNECTION_H */
--- src/server.c
+++ src/server.c
@@ -3,12 +3,33 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <sys/un.h>
+#include <pthread.h>
 #include "connection.h"
+
+void *client_handler(void *arg) {
+    int client_sock = *((int *)arg);
+    char message[MESSAGE_SIZE];
+
+    for (;;) {
+        ssize_t bytes_received = recv(client_sock, message, sizeof(message), 0);
+        if (bytes_received == -1) {
+            perror("receive failed");
+            close(client_sock);
+            pthread_exit(NULL);
+        } else if (bytes_received == 0) {
+            printf("Client disconnected.\n");
+            close(client_sock);
+            pthread_exit(NULL);
+        } else {
+            printf("Message from client: %s\n", message);
+            // Handle the received message here as needed
+        }
+    }
+}
 
 int main() {
     int status = remove(SOCKET_NAME);
-
-    if( status == 0 )
+    if (status == 0)
         printf("%s file deleted successfully.\n", SOCKET_NAME);
 
     int serv_sock, client_sock;
@@ -23,15 +44,17 @@ int main() {
     addr.sun_family = AF_UNIX;
     strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
 
-    if (bind(serv_sock, (const struct sockaddr *) &addr, sizeof(addr)) == -1) {
+    if (bind(serv_sock, (const struct sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(serv_sock, 5) == -1) {
+    if (listen(serv_sock, SOCKETS_AVAILABLE) == -1) {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
+
+    pthread_t thread_id;
     for (;;) {
         client_sock = accept(serv_sock, NULL, NULL);
         if (client_sock == -1) {
@@ -39,17 +62,18 @@ int main() {
             exit(EXIT_FAILURE);
         }
 
-        for (;;) {
-            char message[MESSAGE_SIZE];
-            if (recv(client_sock, message, sizeof(message), 0) == -1) {
-                perror("recieve failed");
-                exit(EXIT_FAILURE);
-            }
-            printf("mesaj de la client: %s\n", message);
-
-            memset(message, 0, sizeof(message));
+        if (pthread_create(&thread_id, NULL, client_handler, (void *)&client_sock) != 0) {
+            perror("pthread_create failed");
+            close(client_sock);
         }
-        close(client_sock);
+
+        if (pthread_detach(thread_id) != 0) {
+            perror("pthread_detach failed");
+            close(client_sock);
+        }
     }
+
     close(serv_sock);
+
+    return 0;
 }
