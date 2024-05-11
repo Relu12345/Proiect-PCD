36b9bc6827f95da42b5f913687c3f6c66a2337eb
Eoczi <koczi.eduard2311@gmail.com>
2024-05-11 22:32:30 +0300

    register with database

--- include/login.h
+++ include/login.h
@@ -1,4 +1,5 @@
 #include <libpq-fe.h>
+#include <stdbool.h>
 
 #ifndef LOGIN_H
 #define LOGIN_H
@@ -6,6 +7,7 @@
 void disableEcho();
 void enableEcho();
 int login(PGconn *conn, char *username, char *password);
+bool ps_register(PGconn *conn, char *username, char *password);
 int create_user(char *username, char *password);
 void processClientInfo(const char* message, char* username, char* password);
 
